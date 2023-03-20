bd7ea8a83ff86ce33849a7a17284898073a15175
schaefertim <tim@gruber-schaefer.de>
2023-03-20 16:54:09 +0100

    Analyze EEG response to changes in synaptic density in model (exp006) and experiment (exp005).

---
+++ exp/exp005_compare_EEG.py
@@ -0,0 +1,59 @@
+"""Compare measured EEG to model prediction.
+
+Plot relative power of model vs relative model of measured EEG for each patient.
+"""
+import pandas as pd
+from matplotlib import pyplot as plt
+
+EEG_measured = pd.read_csv(
+    "/home/tim/dynamic_causal_modeling/data/EEG_SynDens_v2_power_Measured_synDens_PSD95.txt",
+    sep="\t",
+)
+# fix donor labels
+EEG_measured["donor"] = EEG_measured["donor"].apply(lambda donor: donor.replace("0", "").upper())
+
+# %%
+model_measured = pd.read_csv(
+    "/home/tim/dynamic_causal_modeling/results/result_measured.txt", sep="\t"
+)
+list_quantities = ["absPower_normalized", "absPower", "relPower", "relPower_normalized"]
+frequencies = ['delta', 'theta', 'alpha', 'beta', 'gamma1', 'gamma2', 'lowFreq', 'hiFreq', 'totalAbsPow']
+
+# %% common labels
+labels_EEG = set(EEG_measured["donor"])
+labels_model = set(model_measured["donor"])
+labels_common = list(labels_EEG.intersection(labels_model))
+print("common labels", labels_common)
+print("residual EEG", labels_EEG - labels_model)
+print("residual model", labels_model - labels_EEG)
+del labels_EEG, labels_model
+
+# %% plot quantities against each other
+fig, axs = plt.subplots(
+    ncols=len(labels_common),
+    nrows=len(frequencies) - 1,
+    sharex="all",
+    sharey="all",
+    figsize=(10, 10),
+    constrained_layout=True,
+)
+fig.suptitle("Relative power comparison")
+for i_donor, donor in enumerate(labels_common):
+    axs[0, i_donor].set_title(donor)
+    axs[-1, i_donor].set_xlabel("rel. power\nmodel")
+    for i_frequ, frequ in enumerate(frequencies[:-1]):
+        EEG_measured_reduced = EEG_measured[
+            (EEG_measured["donor"] == donor) &
+            (EEG_measured["relative"] == True)  # noqa: E712
+        ]
+        ax = axs[i_frequ, i_donor]
+        ax.plot([0, 1], [0, 1], linestyle="--", color="black")
+        ax.errorbar(
+            model_measured.loc[model_measured["donor"] == donor, f"relPower_{frequ}"],
+            EEG_measured_reduced[frequ[0].upper()+frequ[1:]].mean(),
+            yerr=EEG_measured_reduced[frequ[0].upper()+frequ[1:]].std(),
+            marker='x',
+        )
+        if i_donor == 0:
+            ax.set_ylabel(f"{frequ}\nrel. power\nmeasured")
+fig.show()
---
+++ exp/exp006_plot_model.m
@@ -0,0 +1,52 @@
+% For each synaptic density compute and plot relative power.
+% Result: relationship is almost linear for all frequency bands.
+
+% load model
+[M, pE] = load_cmc_2017_and_priors();
+M.Hz = 1:0.25:96;
+
+% load frequency definitions
+frequency_definitions = get_frequency_definitions();
+freq_bands = fieldnames(frequency_definitions);
+
+% list of synaptic density values
+synapticDensity = 0.5:0.025:1.5;
+power_abs = zeros(size(synapticDensity,2),size(freq_bands,1));
+power_rel = zeros(size(synapticDensity,2),size(freq_bands,1));
+
+% run model for each parameter
+for i_param=1:size(synapticDensity,2)
+    % modulate synaptic gain based on database
+    P = pE;
+    factor = log(synapticDensity(i_param));
+    [M,P] = modify_cmc_2017(M,P,'all-excitatory',factor);
+    
+    % compute power spectrum
+    [freq, psd, psd_normalised, psd_fit] = spm_get_power_spectrum_and_normalization(M,P);
+
+    % compute absolute power
+    power_abs_norm = struct();
+    for i_freq_band=1:numel(freq_bands)
+        band_name = freq_bands{i_freq_band};
+        band_range = frequency_definitions.(band_name);
+        % TODO clarify <= or <
+        selection = (freq >= band_range(1)) & (freq < band_range(2));
+        power_abs(i_param,i_freq_band) = sum(psd(selection));
+    end
+
+    % compute relative power
+    for i_freq_band=1:numel(freq_bands)
+        band_name = freq_bands{i_freq_band};
+        power_rel(i_param,i_freq_band) = power_abs(i_param,i_freq_band) / power_abs(i_param,end);
+    end
+end
+
+% plot
+figure
+for i_freq_band=1:numel(freq_bands)
+    plot(synapticDensity, power_rel(:,i_freq_band)), hold on
+end
+title('Relative power in model')
+xlabel("Synaptic gain modulation factor"), ylabel("relative power");
+legend(freq_bands)
+shg
